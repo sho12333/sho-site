500b1006239f2d35b828be9b6a7c34198fd9fc38
sho <miomini3715@gmail.com>
2023-06-13 22:07:03 +0900

    'アニメーションの追加'

--- public/js/script.js
+++ public/js/script.js
@@ -11,51 +11,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
 let slideIndex = 0;
 let slideTime = 4000; // スライドが切り替わる時間（ミリ秒）
-
-function autoCarousel() {
-  let i;
-  let slides = document.querySelectorAll("[data-carousel-item]");
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slideIndex++;
-
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-
-  setTimeout(autoCarousel, slideTime); // 次のスライドへの切り替えをスケジュール
-}
-
-autoCarousel(); // スクリプトのロード時にカルーセルを開始
+let timer;
 
 let slides = document.querySelectorAll("[data-carousel-item]");
 
 function showSlide(n) {
-  if (n > slides.length) {
-    slideIndex = 1;
+  if (n >= slides.length) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    slideIndex = n;
   }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+
   for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].classList.add("opacity-0");
+    slides[i].classList.remove("opacity-100");
+    slides[i].classList.remove("translate-x-0");
+    slides[i].classList.add("-translate-x-full");
   }
-  slides[slideIndex - 1].style.display = "block";
+
+  slides[slideIndex].classList.remove("opacity-0");
+  slides[slideIndex].classList.add("opacity-100");
+  slides[slideIndex].classList.remove("-translate-x-full");
+  slides[slideIndex].classList.add("translate-x-0");
+}
+
+function autoCarousel() {
+  showSlide(slideIndex + 1);
+  timer = setTimeout(autoCarousel, slideTime); // 次のスライドへの切り替えをスケジュール
 }
 
 function nextSlide() {
-  showSlide((slideIndex += 1));
-  slideTime = 4000;
+  clearTimeout(timer);
+  showSlide(slideIndex + 1);
+  timer = setTimeout(autoCarousel, slideTime);
 }
 
 function prevSlide() {
-  showSlide((slideIndex -= 1));
-  slideTime = 4000;
+  clearTimeout(timer);
+  showSlide(slideIndex - 1);
+  timer = setTimeout(autoCarousel, slideTime);
 }
 
 // イベントリスナーを追加
@@ -68,3 +64,4 @@ document
 
 // 初期スライドを表示
 showSlide(slideIndex);
+autoCarousel(); // スクリプトのロード時にカルーセルを開始
